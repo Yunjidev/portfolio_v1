1dbcbd29372a0087ff9487e27fe59926ba14c3d3
Yunjidev <florian.vancamp@gmail.com>
2023-11-01 09:23:33 +0100

    localstorage

--- src/components/NightModeContext.jsx
+++ src/components/NightModeContext.jsx
@@ -1,12 +1,20 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const NightModeContext = createContext();
 
 export const NightModeProvider = ({ children }) => {
-  const [isNightMode, setNightMode] = useState(false);
+  // Utilisez le localStorage pour obtenir la valeur initiale ou par défaut à false
+  const initialNightMode = JSON.parse(localStorage.getItem('nightMode')) || false;
+
+  const [isNightMode, setNightMode] = useState(initialNightMode);
+
+  useEffect(() => {
+    // Enregistrez l'état actuel du mode nuit dans le localStorage
+    localStorage.setItem('nightMode', JSON.stringify(isNightMode));
+  }, [isNightMode]);
 
   const toggleNightMode = () => {
-    setNightMode(prevMode => !prevMode);
+    setNightMode((prevMode) => !prevMode);
   };
 
   return (
@@ -19,4 +27,3 @@ export const NightModeProvider = ({ children }) => {
 export const useNightMode = () => {
   return useContext(NightModeContext);
 };
-
